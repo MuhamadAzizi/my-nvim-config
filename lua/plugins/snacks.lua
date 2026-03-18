a6c940cc98be987c469707ca54aac5920d60133a
Muhamad Azizi <muhamadazizi177@yahoo.com>
2026-03-18 21:46:48 +0700

    feat(plugin): add snacks.nvim plugin, change terminal to floting mode

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+-- this is for terminal things
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          position = "float",
+        },
+      },
+    },
+  },
+}
